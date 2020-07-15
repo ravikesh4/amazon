428bbc1b8e3b1eb44c81362e8646a0284ddd8bd5
Ravikesh Yadav <ravikesh@netzary.com>
2020-07-15 17:28:21 +0530

    Updates

---
+++ precache-manifest.b4a21c3a91cdf4888bb4578a437d513c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd62af7d5e15ad4d7d466aadb40f97f2",
+    "url": "/amazon/index.html"
+  },
+  {
+    "revision": "6a0c9bc4f5bc3477e54c",
+    "url": "/amazon/static/css/main.cf117b41.chunk.css"
+  },
+  {
+    "revision": "c5dc5e01c3f3e9a7c4ca",
+    "url": "/amazon/static/js/2.034d3190.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/amazon/static/js/2.034d3190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a0c9bc4f5bc3477e54c",
+    "url": "/amazon/static/js/main.9abb2349.chunk.js"
+  },
+  {
+    "revision": "bf5e2e0c760cd7aa92cd",
+    "url": "/amazon/static/js/runtime-main.c52d3dae.js"
+  }
+]);
